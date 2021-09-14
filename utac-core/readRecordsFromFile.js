d37ce57010b3275abb6a7d972226a6a629be1dbb
acailly <ylliac14@gmail.com>
2021-09-14 15:28:59 +0200

    rend la regex de parsing de fichier compatible avec CRLF

--- utac-core/readRecordsFromFile.js
+++ utac-core/readRecordsFromFile.js
@@ -3,8 +3,7 @@ const fs = require("fs").promises;
 module.exports = async function (filePath) {
   const data = await fs.readFile(filePath, "utf8");
 
-  // TODO! N'a pas l'air d'être cross platform à cause des caractères de fin de ligne CRLF / LF
-  const splitRegex = /^\n|\n\n*\n/;
+  const splitRegex = /^\r?\n|\r?\n\r?\n*\r?\n/;
   const records = data.split(splitRegex);
 
   return records;
